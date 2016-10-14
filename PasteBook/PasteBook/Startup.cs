8414a058c908f30e306e069ef46ada991e19138e
Kathy <katrina.ayeng@pointwest.com.ph>
2016-10-14 14:16:03 +0800

    Hello

---
+++ PasteBook/PasteBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PasteBook.Startup))]
+namespace PasteBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
